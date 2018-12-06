679b85cb0ec9997af5febdc0734b417ee7e01145
Vache77 <45490771+Vache77@users.noreply.github.com>
2018-12-06 20:25:46 +0400

    Add files via upload

---
+++ Project/pr2/mardaker_class.js
@@ -0,0 +1,96 @@
+class Mardaker extends LivingCreature {
+    constructor(x, y, index) {
+        super(x,y,index);
+        this.energy = 15;
+    }
+    getNewDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        this.getNewDirections()
+        return super.chooseCell(character);
+    }
+    mult() {
+        var empty = random(this.chooseCell(0))
+        if (empty && this.energy > 30) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 4
+            var mardaker = new Mardaker(newX, newY, 5)
+            mardakerArr.push(mardaker)
+        }
+    }
+    move() {
+        var empty = random(this.chooseCell(0));
+        this.energy -= 3
+        if (empty) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 4
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        }
+    }
+    move1() {
+
+        var empty1 = random(this.chooseCell(1));
+        this.energy -= 3
+
+        if (empty1) {
+
+            var newX = empty1[0]
+            var newY = empty1[1]
+            matrix[newY][newX] = 5
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in grassArr) {
+                if (grassArr[i].x == newX && grassArr[i].y == newY) {
+                    grassArr.splice(i, 1)
+                }
+            }
+        }
+
+    }
+    eat() {
+        var food = random(this.chooseCell(4))
+
+        if (food) {
+            var newX = food[0]
+            var newY = food[1]
+            matrix[newY][newX] = 5
+
+            matrix[this.y][this.x] = 0
+            for (var i in mardArr) {
+                if (mardArr[i].x == newX && mardArr[i].y == newY) {
+                    mardArr.splice(i, 1)
+                }
+            }
+            this.x = newX
+            this.y = newY
+            this.energy += 5
+        }
+    }
+
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0;
+            for (var i in mardakerArr) {
+                if (mardakerArr[i].x == this.x && mardakerArr[i].y == this.y) {
+                    mardakerArr.splice(i, 1)
+                }
+            }
+        }
+    }
+
+}
---
+++ Project/pr2/xotaker_class.js
@@ -0,0 +1,75 @@
+class Xotaker extends LivingCreature {
+    constructor(x,y,index) {  
+        super(x,y,index);
+        this.energy=10;
+    }
+    getNewDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        this.getNewDirections()
+        return super.chooseCell(character);
+    }
+    mult() {
+        var empty = random(this.chooseCell(0))
+
+        if (empty && this.energy > 10) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 2
+            var xt = new Xotaker(newX, newY, 2)
+            xotakerArr.push(xt)
+        }
+    }
+    move() {
+        var empty = random(this.chooseCell(0))
+        if (empty) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 2
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+
+        }
+        this.energy--
+    }
+    eat() {
+        var food = random(this.chooseCell(1))
+        if (food) {
+            var newX = food[0]
+            var newY = food[1]
+            matrix[newY][newX] = 2
+            matrix[this.y][this.x] = 0
+            for (var i in grassArr) {
+                if (grassArr[i].x == newX && grassArr[i].y == newY) {
+                    grassArr.splice(i, 1)
+                }
+            }
+            this.x = newX
+            this.y = newY
+            this.energy += 3
+        }
+    }
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0
+            for (var i in xotakerArr) {
+                if (xotakerArr[i].x == this.x && xotakerArr[i].y == this.y) {
+                    xotakerArr.splice(i, 1)
+                }
+            }
+        }
+    }
+
+
+}
